4b911d937baa476996c2c364f0998e666d998117
JohnSalimRobert <121552921+JohnSalimRobert@users.noreply.github.com>
2023-06-01 19:42:23 +0500

    modified:   frontend/src/Ecommerce_banners/IMG_1.gif 	new file:   frontend/src/Ecommerce_banners/IMG_2.gif 	new file:   frontend/src/Ecommerce_banners/IMG_3.gif 	new file:   frontend/src/Ecommerce_banners/IMG_4.gif 	new file:   frontend/src/Ecommerce_banners/IMG_5.gif 	deleted:    frontend/src/Ecommerce_banners/img.jpg 	new file:   frontend/src/Ecommerce_banners/img12.gif 	renamed:    frontend/src/Ecommerce_banners/IMG_2.jpg -> frontend/src/Ecommerce_banners/img2.jpg 	renamed:    frontend/src/Ecommerce_banners/IMG_3.jpg -> frontend/src/Ecommerce_banners/img3.jpg 	renamed:    frontend/src/Ecommerce_banners/IMG_4.jpg -> frontend/src/Ecommerce_banners/img4.jpg 	renamed:    frontend/src/Ecommerce_banners/IMG_5.jpg -> frontend/src/Ecommerce_banners/img5.jpg 	modified:   frontend/src/components/Carousel.js

--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -1,34 +1,51 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import IMG1 from "../Ecommerce_banners/IMG_1.gif";
-import IMG2 from "../Ecommerce_banners/IMG_2.jpg";
-import IMG3 from "../Ecommerce_banners/IMG_3.jpg";
-import IMG4 from "../Ecommerce_banners/IMG_4.jpg";
-import IMG5 from "../Ecommerce_banners/IMG_5.jpg";
-import IMG6 from "../Ecommerce_banners/IMG_6.jpg";
-import IMG7 from "../Ecommerce_banners/IMG_7.jpg";
-import IMG8 from "../Ecommerce_banners/IMG_8.jpg";
-import IMG9 from "../Ecommerce_banners/IMG_9.jpg";
+import IMG2 from "../Ecommerce_banners/IMG_2.gif";
+import IMG3 from "../Ecommerce_banners/IMG_3.gif";
+import IMG4 from "../Ecommerce_banners/IMG_4.gif";
+import IMG5 from "../Ecommerce_banners/IMG_5.gif";
+import { Link } from "react-router-dom";
+// import IMG6 from "../Ecommerce_banners/IMG_6.jpg";
+// import IMG7 from "../Ecommerce_banners/IMG_7.jpg";
+// import IMG8 from "../Ecommerce_banners/IMG_8.jpg";
+// import IMG9 from "../Ecommerce_banners/IMG_9.jpg";
 
-export default function Carousel1 () {
-    return(
-<Carousel showArrows autoPlay showThumbs={false}>
-    <div className="carouselImg">
-        <img alt="" src={IMG1} />
-    </div>
-    <div className="carouselImg">
-    <img alt="" src={IMG2} />
-    </div>
-    <div className="carouselImg">
-    <img alt="" src={IMG3} />
-    </div>
-    <div className="carouselImg">
-    <img alt="" src={IMG4} />
-    </div>
-    <div className="carouselImg">
-    <img alt="" src={IMG5} />
-    </div>
-    <div className="carouselImg">
+export default function Carousel1() {
+  return (
+    <Carousel
+      showArrows
+      autoPlay
+      showThumbs={false}
+      infiniteLoop={true}
+      autoFocus
+    >
+      <Link to={`/search/category/64039044da7e7e7b69e26024`}>
+        <div className="carouselImg">
+          <img alt="" src={IMG1} />
+        </div>
+      </Link>
+      <Link to={`/search/category/6404a68a5ed7705ead15ebe2`}>
+      <div className="carouselImg">
+        <img alt="" src={IMG2} />
+      </div>
+      </Link>
+      <Link to={`/search/category/6404ab166b71954cd5434dd6`}>
+      <div className="carouselImg">
+        <img alt="" src={IMG3} />
+      </div>
+      </Link>
+      <Link to={`/search/category/642f731a58748f0355532f8f`}>
+      <div className="carouselImg">
+        <img alt="" src={IMG4} />
+      </div>
+      </Link>
+      <Link to={`/search/category/642f747b58748f0355532fa3`}>
+      <div className="carouselImg">
+        <img alt="" src={IMG5} />
+      </div>
+      </Link>
+      {/* <div className="carouselImg">
     <img alt="" src={IMG6} />
     </div>
     <div className="carouselImg">
@@ -39,6 +56,7 @@ export default function Carousel1 () {
     </div>
     <div className="carouselImg">
     <img alt="" src={IMG9} />
-    </div>
-    </Carousel>)
-    }
+    </div> */}
+    </Carousel>
+  );
+}
